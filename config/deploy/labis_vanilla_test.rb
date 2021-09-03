e9a4f52fced7932025ec8bafdb150f9945ac95bd
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-09-03 17:10:39 -0500

    modify deploy

--- config/deploy/labis_vanilla_test.rb
+++ config/deploy/labis_vanilla_test.rb
@@ -1,6 +1,6 @@
-set :stage, :labis_test
-set :rails_env, :labis_test
+set :stage, :labis_vanilla_test
+set :rails_env, :labis_vanilla_test
 set :branch, "main"
 set :d_enable, false
 
-server "10.212.148.50", user: "rails", roles: %w{app db web}
+server "10.208.42.130", user: "rails", roles: %w{app db web}
