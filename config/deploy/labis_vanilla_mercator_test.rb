43d13ef1722c6af285f73364201197fcdd355f00
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-09-07 16:02:11 -0500

    add mercator database deploy

---
+++ config/deploy/labis_vanilla_mercator_test.rb
@@ -0,0 +1,6 @@
+set :stage, :labis_vanilla_mercator_test
+set :rails_env, :labis_vanilla_mercator_test
+set :branch, "main"
+set :d_enable, false
+
+server "10.208.42.130", user: "rails", roles: %w{app db web}
