eba5701cf75c1ea0f7de616b6ab127416918508c
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-09-07 10:56:10 -0500

    add node

--- bin/docker/entrypoint_labis_test.sh
+++ bin/docker/entrypoint_labis_test.sh
@@ -4,4 +4,4 @@ APP_ENV=labis_vanilla_test
 SERVICE=labis_db_seed
 DIR=/var/www/$SERVICE/current
 
-su - rails -c "cd $DIR && RAILS_ENV=$APP_ENV rails db:data_load"
+su - rails -c "cd $DIR && RAILS_ENV=$APP_ENV rake db:data_load"
