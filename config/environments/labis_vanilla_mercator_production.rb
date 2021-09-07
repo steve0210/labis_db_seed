dc7072a54854f24c43d8ea9d235ba56002958c72
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-09-07 15:22:22 -0500

    add mercator database tables

---
+++ config/environments/labis_vanilla_mercator_production.rb
@@ -0,0 +1,7 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+end
+
